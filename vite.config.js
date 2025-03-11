4c87be38f1ba49722ae90a39e300e58e994b5059
Виталий <vitaliy-yakovlev@ukr.net>
2025-03-11 18:20:48 +0100

    fixed config vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
         svgo: false,
         titleProp: true,
       },
-      include: '**/*.svg',
+      // include: '**/*.svg',
     }),
   ],
   build: {
