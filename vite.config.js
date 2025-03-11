e42952a62d5c7741f4f0b1043a912f989ce6c31e
Виталий <vitaliy-yakovlev@ukr.net>
2025-03-11 17:46:05 +0100

    create new project

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
+});
